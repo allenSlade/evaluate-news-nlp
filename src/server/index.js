851f6dc9e39b9776d7b5059b3b06aa492105d1db
Allen Slade <allen.slade@gmail.com>
2020-08-04 19:48:39 -0700

    Add text API

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
+
+var aylienapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
+
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 const app = express()
 
