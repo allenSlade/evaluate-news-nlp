826267d1c21b84cff3438ec1d13a17de9010c889
Allen Slade <allen.slade@gmail.com>
2020-08-10 20:39:36 -0700

    Add API calls

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,48 @@
+// function handleSubmit(event) {
+//     event.preventDefault()
+//
+//     // check what text was put into the form field
+//     let formText = document.getElementById('name').value
+//     Client.checkForName(formText)
+//
+//     console.log("::: Form Submitted :::")
+//     fetch('http://localhost:8081/test')
+//     .then(res => res.json())
+//     .then(function(res) {
+//         document.getElementById('results').innerHTML = res.message
+//     })
+// }
+
+// export { handleSubmit }
+
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+  let formText = document.getElementById('name').innerText
+  Client.checkForName(formText)
+  console.log("Form Submitted!")
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  fetch('https://api.aylien.com/news')
+  .then(res => res.json())
+  .then(function(res) {
+      document.getElementById('results').innerHTML = res.message
+  })
 }
 
 export { handleSubmit }
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,69 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-var path = require('path')
+let path = require('path')
 const express = require('express')
+const cors = require('cors')
 const mockAPIResponse = require('./mockAPI.js')
-var aylien = require("aylien_textapi");
+// let aylien = require("aylien_textapi");
 
-var aylienapi = new aylien({
+// textapi.sentiment({
+//   'text': 'John is a very good football player!'
+// }, function(error, response) {
+//   if (error === null) {
+//     console.log(response);
+//   }
+// });
+var AylienNewsApi = require("aylien-news-api");
+
+var defaultClient = AylienNewsApi.ApiClient.instance;
+
+var app_id = defaultClient.authentications["app_id"];
+app_id.apiKey = process.env["NEWSAPI_APP_ID"];
+
+var app_key = defaultClient.authentications["app_key"];
+app_key.apiKey = process.env["NEWSAPI_APP_KEY"];
+
+var api = new AylienNewsApi.DefaultApi();
+
+var opts = {
+  title: "trump",
+  sortBy: "social_shares_count.facebook",
+  notLanguage: ["en"],
+  publishedAtStart: "NOW-7DAYS",
+  publishedAtEnd: "NOW",
+  entitiesBodyLinksDbpedia: [
+    "http://dbpedia.org/resource/Donald_Trump",
+    "http://dbpedia.org/resource/Hillary_Rodham_Clinton"
+  ]
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("API called successfully. Returned data: ");
+    console.log("========================================");
+    for (var i = 0; i < data.stories.length; i++) {
+      console.log(data.stories[i].title + " / " + data.stories[i].source.name);
+    }
+  }
+};
+
+api.listStories(opts, callback);
+
+let textapi = new aylien({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY
 });
 
+
+
 console.log(`Your API key is ${process.env.API_KEY}`);
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.static('dist'))
 
@@ -32,3 +82,18 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+let projectData = {};
+
+app.post('/post', function (req, res) {
+console.log('Response:', req.body);
+// let newEntry = {
+//     date: req.body.date,
+//     temp: req.body.temp,
+//     content: req.body.content
+//   }
+//   projectData = newEntry;
+  console.log(projectData);
+  res.send(projectData);
+});
